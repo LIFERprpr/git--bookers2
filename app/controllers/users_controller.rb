b91976add3fa056f7b7b9de193712e07f1089cfc
EC2 Default User <ec2-user@ip-172-31-88-186.ec2.internal>
2021-07-26 11:13:05 +0000

    [add}コントローラやモデルの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+end
