cac36ca35a2e9aee86ed5cb39af972cedabebcb8
EC2 Default User <ec2-user@ip-172-31-88-186.ec2.internal>
2021-07-27 13:06:28 +0000

    [Add]viewページなどの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,22 @@
 class BooksController < ApplicationController
   
   def index
+    @books = Book.all
+    @book = Book.new
+    
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+    
+    
   end
   
   def show
+    
   end
   
   def edit
@@ -16,6 +26,12 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
