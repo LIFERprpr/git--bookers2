628c376f497a269d14450af6de2891da1129c5ff
EC2 Default User <ec2-user@ip-172-31-88-186.ec2.internal>
2021-07-28 06:12:51 +0000

    [Add]viewページの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,39 +1,36 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
-    
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
-    
-    
   end
-  
+
   def show
+     #<Book id: 1, title: "test", body: "test", created_at: "2021-07-28 05:03:46", updated_at: "2021-07-28 05:03:46", user_id: 1>
     @book = Book.find(params[:id])
-    @books = Book.all
-    
+     #<User id: 1, email: "dmmweb@gmail.com", name: "dmm", created_at: "2021-07-26 08:45:07", updated_at: "2021-07-26 08:45:07", introduction: nil, profile_image_id: nil>
+    @user = @book.user
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,26 @@
 class UsersController < ApplicationController
-  
+
   def index
   end
 
   def show
+    #<User id: 1, email: "dmmweb@gmail.com", name: "dmm", created_at: "2021-07-26 08:45:07", updated_at: "2021-07-26 08:45:07", introduction: nil, profile_image_id: nil>
     @user = User.find(params[:id])
-    @books = Book.all
-    
+    #<ActiveRecord::Associations::CollectionProxy [#<Book id: 1, title: "test", body: "test", created_at: "2021-07-28 05:03:46", updated_at: "2021-07-28 05:03:46", user_id: 1>]>
+    @books = @user.books
+    @book = Book.new
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
 end
